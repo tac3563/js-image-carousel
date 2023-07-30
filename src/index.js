8e6856a027fb1ccaf12185d53c2c6ba03fe0a6b0
tac3563 <tcox_bu@outlook.com>
2023-07-30 13:38:46 +0100

    [Update] added new placeholders and added functionality for slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./styles/main.scss";
+
+const slider = document.querySelectorAll('.project');
+const sliderNext = document.querySelector('.next');
+const sliderPrev = document.querySelector('.prev');
+
+
+function toggleActiveClass() {
+    const activeSlide = document.querySelector('.project.active');
+    let targetSlide;
+
+    if (this === sliderNext) {
+        targetSlide = activeSlide.nextElementSibling || slider[0]; 
+    } else if (this === sliderPrev) {
+        targetSlide = activeSlide.previousElementSibling || slider[slider.length - 1]; 
+    }
+   
+    activeSlide.classList.toggle('active');
+    activeSlide.classList.toggle('hidden');
+    targetSlide.classList.toggle('active');
+    targetSlide.classList.toggle('hidden');
+}
+
+
+
+sliderNext.addEventListener( 'click', toggleActiveClass);
+sliderPrev.addEventListener( 'click', toggleActiveClass);
