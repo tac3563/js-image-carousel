64656af1beaebd5266ec6b9a7763ceb6c107aa34
tac3563 <tcox_bu@outlook.com>
2023-08-01 23:06:50 +0100

    [Update] added left or right animation depending on prev/next button click

--- src/index.js
+++ src/index.js
@@ -7,18 +7,27 @@ const sliderPrev = document.querySelector('.prev');
 function toggleActiveClass() {
     const activeSlide = document.querySelector('.project.active');
     let targetSlide;
+    let direction;
 
     if (this === sliderNext) {
         targetSlide = activeSlide.nextElementSibling || slider[0]; 
+        direction = 'left'
+      
     } else if (this === sliderPrev) {
         targetSlide = activeSlide.previousElementSibling || slider[slider.length - 1]; 
+        direction = 'right'
     }
    
-    activeSlide.classList.toggle('active');
+    targetSlide.classList.add(`${direction}`, 'active');
     activeSlide.classList.toggle('hidden');
-    targetSlide.classList.toggle('active');
+    activeSlide.classList.toggle('active');
     targetSlide.classList.toggle('hidden');
+
+    setTimeout(() => {
+        targetSlide.classList.remove(`${direction}`);
+      }, 1200);
 }
+
 
 sliderNext.addEventListener( 'click', toggleActiveClass);
 sliderPrev.addEventListener( 'click', toggleActiveClass);
