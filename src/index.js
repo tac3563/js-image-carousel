59695e1d120334c9cee0a85a7d4744d497c2f89b
tac3563 <tom.cox@cite.co.uk>
2023-08-07 21:01:26 +0100

    [Update] added carousel items styles and ensured that each item kept styles when prev/next buttons clicked

--- src/index.js
+++ src/index.js
@@ -26,8 +26,36 @@ function toggleActiveClass() {
     setTimeout(() => {
         targetSlide.classList.remove(`${direction}`);
       }, 1200);
-}
 
+    const sliderArray = [...slider];
+    const activeSlideIndex = sliderArray.findIndex((slide) => slide.classList.contains('active'));
+
+    sliderArray.forEach((item, index) => {
+        item.classList.remove('furthest-left', 'left-index', 'right-index', 'furthest-right');
+
+        if (index === activeSlideIndex - 2) {
+            item.classList.add('furthest-left');
+        } else if (index === activeSlideIndex - 1) {
+            item.classList.add('left-index');
+        } else if (index === activeSlideIndex + 1) {
+            item.classList.add('right-index');
+        } else if (index === activeSlideIndex + 2) {
+            item.classList.add('furthest-right');
+        }
+
+        if (activeSlideIndex === 0) {
+            sliderArray[sliderArray.length - 1].classList.add('furthest-left');
+            sliderArray[sliderArray.length - 2].classList.add('left-index');
+        } else if (activeSlideIndex === 1) {
+            sliderArray[sliderArray.length - 1].classList.add('furthest-left');
+        } else if (activeSlideIndex === 3) {
+            sliderArray[sliderArray.length - 5].classList.add('furthest-right');
+        } else if (activeSlideIndex === 4) {
+            sliderArray[sliderArray.length - 5].classList.add('right-index');
+            sliderArray[sliderArray.length - 4].classList.add('furthest-right');
+        }
+    });
+}
 
 sliderNext.addEventListener( 'click', toggleActiveClass);
 sliderPrev.addEventListener( 'click', toggleActiveClass);
